ed6e3c2c60583baabb989bd74c1c216a7ad5a873
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-28 17:03:45 -0500

    some more simple stuff

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -9,15 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
     alert('Click Off $%$*(%!')
     });
 });
-var butt = document.createElement("butt");
-butt.innerHTML = "More butt-ons";
-document.body.appendChild(butt);
-butt.addEventListener("click", function () {
-    alert("Nique"), function () {
-        document.createElement('span');
-        span.butt = ()
-    };    
-});
+ 
+function myFunction() {
+    var x = document.getElementById('anotherfox');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
 // 4. Put some text in a paragraph. Make it where when you click on the paragraph, the color of the text switches to red. Once you get that working, try to rewrite your code to make it switch to a random color each click (you don't have to show the code for just red once you get random working).
 
 // 5. Add a button and an empty div. When the button is clicked, add a `span` that contains your name to the empty div.
